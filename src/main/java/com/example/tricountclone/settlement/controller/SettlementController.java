50fbb144f951e69458c219dcf08cfdfaa7e9bcb8
Lee <94841127+galaxian@users.noreply.github.com>
2024-03-19 00:24:09 +0900

    feat: Get /settlement/{id} 정산 조회 api 컨트롤러 구현

--- src/main/java/com/example/tricountclone/settlement/controller/SettlementController.java
+++ src/main/java/com/example/tricountclone/settlement/controller/SettlementController.java
@@ -3,6 +3,7 @@ package com.example.tricountclone.settlement.controller;
 import java.net.URI;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.tricountclone.member.security.MemberContext;
 import com.example.tricountclone.settlement.dto.reqest.CreateSettlementReqDto;
+import com.example.tricountclone.settlement.dto.response.FindSettlementResDto;
 import com.example.tricountclone.settlement.service.SettlementService;
 
 import lombok.RequiredArgsConstructor;
@@ -30,5 +32,11 @@ public class SettlementController {
 	public ResponseEntity<Void> joinSettlement(@PathVariable("id") Long settlementId) {
 		settlementService.joinSettlement(settlementId, MemberContext.getCurrentMember());
 		return ResponseEntity.created(URI.create("/settlements/" + settlementId)).build();
+	}
+
+	@GetMapping("/settlement/{id}")
+	public ResponseEntity<FindSettlementResDto> findSettlement(@PathVariable("id") Long settlementId) {
+		FindSettlementResDto resDto = settlementService.findSettlement(settlementId, MemberContext.getCurrentMember());
+		return ResponseEntity.ok().body(resDto);
 	}
 }
