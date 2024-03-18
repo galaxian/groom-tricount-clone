c6e60f5c262ef48d4f0b6e870b465f8089c2768f
Lee <94841127+galaxian@users.noreply.github.com>
2024-03-18 22:28:09 +0900

    feat: Get login api 구현

--- src/main/java/com/example/tricountclone/member/controller/MemberController.java
+++ src/main/java/com/example/tricountclone/member/controller/MemberController.java
@@ -3,10 +3,12 @@ package com.example.tricountclone.member.controller;
 import java.net.URI;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.tricountclone.member.dto.request.LoginReqDto;
 import com.example.tricountclone.member.dto.request.SignupReqDto;
 import com.example.tricountclone.member.service.MemberService;
 
@@ -22,5 +24,11 @@ public class MemberController {
 	public ResponseEntity<Void> signup(@RequestBody SignupReqDto reqDto) {
 		memberService.signUp(reqDto);
 		return ResponseEntity.created(URI.create("/signup")).build();
+	}
+
+	@GetMapping("/login")
+	public ResponseEntity<Void> login(@RequestBody LoginReqDto reqDto) {
+		memberService.login(reqDto);
+		return ResponseEntity.created(URI.create("/login")).build();
 	}
 }
