47fb820f801eacd4792b15627169c2ce35727db3
Lee <94841127+galaxian@users.noreply.github.com>
2024-03-18 22:04:49 +0900

    feat: Post /signup api 구현

---
+++ src/main/java/com/example/tricountclone/member/controller/MemberController.java
@@ -0,0 +1,26 @@
+package com.example.tricountclone.member.controller;
+
+import java.net.URI;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.tricountclone.member.dto.request.SignupReqDto;
+import com.example.tricountclone.member.service.MemberService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
+public class MemberController {
+
+	private final MemberService memberService;
+
+	@PostMapping("/signup")
+	public ResponseEntity<Void> signup(@RequestBody SignupReqDto reqDto) {
+		memberService.signUp(reqDto);
+		return ResponseEntity.created(URI.create("/signup")).build();
+	}
+}
