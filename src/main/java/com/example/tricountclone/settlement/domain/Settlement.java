1830254cd17ebcf9a5e3f211d6ef31d548ea4525
Lee <94841127+galaxian@users.noreply.github.com>
2024-03-18 23:47:53 +0900

    feat: Settlement entity 수정

--- src/main/java/com/example/tricountclone/settlement/domain/Settlement.java
+++ src/main/java/com/example/tricountclone/settlement/domain/Settlement.java
@@ -1,18 +1,16 @@
 package com.example.tricountclone.settlement.domain;
 
-import java.util.List;
-
 import com.example.tricountclone.common.BaseEntity;
-import com.example.tricountclone.member.domain.Member;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Entity
 public class Settlement extends BaseEntity {
@@ -23,6 +21,7 @@ public class Settlement extends BaseEntity {
 
 	private String name;
 
-	@OneToMany(mappedBy = "id")
-	private List<Member> memberList;
+	public Settlement(String name) {
+		this.name = name;
+	}
 }
