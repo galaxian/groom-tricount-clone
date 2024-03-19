620fa9a4657b67037b9092fb12128d34b730bf22
Lee <94841127+galaxian@users.noreply.github.com>
2024-03-19 21:57:39 +0900

    feat: Get /expense/{settlementId} 정산 조회 api 컨트롤러 구현

--- src/main/java/com/example/tricountclone/expense/controller/ExpenseController.java
+++ src/main/java/com/example/tricountclone/expense/controller/ExpenseController.java
@@ -1,15 +1,19 @@
 package com.example.tricountclone.expense.controller;
 
 import java.net.URI;
+import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.tricountclone.expense.dto.request.CreateExpenseReqDto;
+import com.example.tricountclone.expense.dto.response.GetBalanceResDto;
 import com.example.tricountclone.expense.service.ExpenseService;
+import com.example.tricountclone.member.domain.Member;
 import com.example.tricountclone.member.security.MemberContext;
 
 import lombok.RequiredArgsConstructor;
@@ -25,5 +29,11 @@ public class ExpenseController {
 		@RequestBody CreateExpenseReqDto reqDto) {
 		expenseService.createExpense(settlementId, reqDto, MemberContext.getCurrentMember());
 		return ResponseEntity.created(URI.create("/expense")).build();
+	}
+
+	@GetMapping("/expense/{settlementId}")
+	public ResponseEntity<List<GetBalanceResDto>> getBalance(@PathVariable("settlementId") Long settlementId) {
+		List<GetBalanceResDto> result = expenseService.getBalance(settlementId, MemberContext.getCurrentMember());
+		return ResponseEntity.ok(result);
 	}
 }
